b2372a7151085e71c0c652fdc2d9c2748c90c66a
oweixx <163521697+oweixx@users.noreply.github.com>
2024-06-06 15:41:41 +0900

    Title: 1로 이루어진 집합의 크기, Time: 8090ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

---
+++ 240606/1로 이루어진 집합의 크기/size-of-set-of-1s.py
@@ -0,0 +1,62 @@
+from collections import deque
+def bfs(i,j) :
+    q = deque()
+    q.append((i,j))
+    check[i][j] = True
+    maps[i][j] = temp
+    count = 1
+    while q :
+        x, y = q.popleft()
+
+        for nx, ny in [(x-1,y), (x+1,y), (x,y-1), (x,y+1)] :
+            if 0<=nx<n and 0<=ny<m and maps[nx][ny] == 1 and not check[nx][ny] :
+                maps[nx][ny] = temp
+                check[nx][ny] = True
+                q.append((nx,ny))
+                count += 1
+
+    sum_count.append(count)
+
+n, m = map(int,input().split())
+
+maps = []
+sum_count = [0,0]
+for _ in range(n) :
+    s = list(map(int,input().split()))
+    maps.append(s)
+
+check = [[False]*m for _ in range(n)]
+temp = 1
+for i in range(n) :
+    for j in range(m) :
+        if maps[i][j] == 1 and not check[i][j] :
+            temp += 1
+            bfs(i,j)
+
+new_count = []
+for i in range(n) :
+    for j in range(m) :
+        if not maps[i][j] :
+            temp = 1
+            check = [False]*len(sum_count)
+            for nx, ny in [(i-1,j), (i+1,j), (i,j-1), (i,j+1)] :
+                if 0<=nx<n and 0<=ny<m and maps[nx][ny] and not check[maps[nx][ny]] :
+                    temp += sum_count[maps[nx][ny]]
+                    check[maps[nx][ny]] = True
+            new_count.append(temp)
+            
+
+#print(sum_count)
+#print(new_count)
+
+result = 0
+if len(new_count) == 0 :
+    if max(sum_count) != n*m :
+        result = max(sum_count) + 1
+    else :
+        result = max(sum_count) - 1
+
+else :
+    result = max(max(sum_count)+1, max(new_count))
+
+print(result)

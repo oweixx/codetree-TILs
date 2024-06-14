b9a8e7c9603ee685e0b88f17f84d0e5c7171c612
oweixx <163521697+oweixx@users.noreply.github.com>
2024-06-14 14:12:11 +0900

    Title: 편안한 핀셋, Time: 689ms, Memory: 41MB, Status: Passed - CodeTree

--- 240614/편안한 핀셋/comfortable-tweezers.py
+++ 240614/편안한 핀셋/comfortable-tweezers.py
@@ -1,21 +1,23 @@
 n = int(input())
+arr = [[False]*1001 for _ in range(1001)]
 check = [[0]*1001 for _ in range(1001)]
 
 count = 0
 for i in range(n) :
     a, b = map(int,input().split())
+    arr[a][b] = True
     temp = 0
     for x,y in [(-1,0), (1,0), (0,-1), (0,1)] :
         nx,ny = a + x, b + y
         if 0<=nx<=1000 and 0<=ny<=1000 :
-            if check[nx][ny] == 3 :
-                count -= 1
             check[nx][ny] += 1
-            if check[nx][ny] == 3 : 
+            if check[nx][ny] == 3 and arr[nx][ny]:
                 count += 1
                 temp += 1
+            if check[nx][ny] > 3 and arr[nx][ny]:
+                count -= 1
 
-    if temp == 3 :
+    if temp == 3:
         count += 1
 
     print(count)
